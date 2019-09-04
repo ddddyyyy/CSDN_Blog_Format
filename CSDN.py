31158c6c172d453629af4c4556ef5621164a8ef9
ddddyyyy <2115601599@qq.com>
2019-09-04 23:54:22 +0800

    修复了csdn的接口挂了之后出现的bug，并完善了使用方法

--- CSDN.py
+++ CSDN.py
@@ -1,8 +1,12 @@
+import argparse
 import json
 import logging
 import logging.handlers
-import math
+import os
+import re
 import urllib.request
+
+from lxml import etree
 
 
 # 删除字典数组重复的元素
@@ -15,13 +19,14 @@ def delete_duplicate(li):
 class CSDN:
     def __init__(self):
         # 输出文件的目录
-        self.output = '/root/Hexo/source/_posts/'
+        self.output = ''
         # 自己的名字
         self.user = 'madonghyu'
 
         self.blog_url = 'https://blog.csdn.net/{0}/article/list/'.format(self.user)
-        # 得到所有文章列表的url
-        self.article_list_url = 'https://blog.csdn.net/{0}/phoenix/article/list/'.format(self.user)
+
+        self.header = [
+            ('Cookie', '')]
 
         # 创建Logger
         self.logger = logging.getLogger()
@@ -37,6 +42,10 @@ class CSDN:
         console_handler.setFormatter(formatter)
         # 添加到Logger中
         self.logger.addHandler(console_handler)
+        # 装载cookie
+        opener = urllib.request.build_opener()
+        opener.addheaders = self.header
+        urllib.request.install_opener(opener)
 
     # 初始化请求header
     def init_header(self, cookie):
@@ -49,29 +58,46 @@ class CSDN:
         urllib.request.install_opener(opener)
 
     def get_article_list(self):
-        i = 1
-        page_num = -1
-        lists = []
-        # 循环遍历
-        while True:
-            response = json.loads(urllib.request.urlopen(self.article_list_url + str(i)).read())
-            if response['status'] is 1:
-                if page_num == -1:
-                    # 得到遍历的次数
-                    page_num = math.ceil(response['data']['total'] / 20)
-                # 得到文章列表
-                article_list = response['data']['article_list']
-                for data in article_list:
-                    # 得到文章的id和创建日期
-                    lists.append({'id': data['ArticleId'], 'data': data['PostTime']})
-            if i > page_num:
-                break
-            i = i + 1
+        # 最终所有文章id和日期的数组
+        result_list = []
 
-        for i in lists:
+        # 获得文章链接，由于页面有一些评论的链接，需要过滤掉
+        r_str = '^https\:\/\/blog\.csdn\.net\/{0}\/article\/details\/[0-9]+$'.format(self.user)
+        url_re = re.compile(r_str)
+        # 替换url得到文章的id
+        replace_re = re.compile('^https://blog\.csdn\.net/.+/article/details/')
+
+        j = 1
+        self.logger.info('开始获取文章总数目')
+        # 这里直接使用循环代替页码的获取了，当检索不到数据的时候跳出循环
+        while True:
+            r = urllib.request.urlopen(self.blog_url + str(j))
+            html = etree.HTML(r.read())
+
+            # 获得所有的链接
+            u_list = html.xpath('//*[@id="mainBox"]/main//a[@href]')
+            id_list = []
+            for uri in u_list:
+                # '//*[@id="mainBox"]/main/div[2]/div[2]/h4/a'
+                # '//*[@id="mainBox"]/main/div[2]/div[2]/div[1]/p[1]/span'
+                ri = url_re.match(uri.attrib['href'])
+
+                if ri is not None:
+                    # 获取文章id
+                    data = uri.getparent().getparent().xpath('div[1]/p[1]/span')[0].text
+                    id_list.append({'id': replace_re.sub('', uri.attrib['href']), 'data': data})
+            temp_list = delete_duplicate(id_list)
+            # temp_list = id_list
+            if len(temp_list) == 0:
+                break
+            j += 1
+            result_list += temp_list
+
+        self.logger.info('总共有%d篇文章' % len(result_list))
+
+        for i in result_list:
             try:
                 self.format_article(i)
-                return
             except Exception as a:
                 self.logger.error(a)
 
@@ -92,16 +118,20 @@ class CSDN:
             c_str += "  - [{0}]\n".format(i)
         tags = j['data']['tags']
         # 得到Hexo的所需的markdown格式
-        string = "---\ntitle: {0}\ndate: {3}\ntags: [{1}]\ncategories:\n{2}\n\n---\n\n<!--more-->\n\n\n\n".format(title,
-                                                                                                                  tags,
-                                                                                                                  c_str,
-                                                                                                                  dic[
-                                                                                                                      'data'])
+        string = "---\ntitle: {0}\ndate: {3}\ntags: [{1}]\ncategories:\n{2}\n\n---\n\n<!--more-->\n\n\n\n" \
+            .format(title,
+                    tags,
+                    c_str,
+                    dic['data'])
+        if self.output[-1] is not '/':
+            self.output += '/'
+        # 创建文件夹
+        if not os.path.exists(self.output):
+            os.mkdir(self.output)
         result = string + markdowncontent
-        file = open(self.output + '{0}.md'.format(title), 'w')
-        file.write(result)
-        file.close()
-        self.logger.info(self.output + '{0}.md'.format(title) + '添加成功！')
+        with open(self.output + '{0}.md'.format(title), 'w') as file:
+            file.write(result)
+            self.logger.info(title + '添加成功！')
 
     def run(self):
         try:
@@ -110,14 +140,40 @@ class CSDN:
             self.logger.error(a)
 
 
-if __name__ == '__main__':
+# 如果觉得每次启动脚本都要输入参数很麻烦，可以直接将下面的代码注释取消，完善csdn.output，csdn.user，csdn.init_header的值既可
+
+# if __name__ == '__main__':
+#     csdn = CSDN()
+#
+#     # 输出路径
+#     csdn.output = './output/'
+#     # 输入自己的CSDN账户名
+#     csdn.user = ''
+#     # CSDN的登录之后的cookie,即UserToken和UserInfo的值，这两个的值都是一样的
+#     # 可以在CSDN的网站下打开浏览器的开发者模式，访问自己的文章页面，查看请求的header得到
+#     csdn.init_header('')
+#
+#     csdn.run()
+
+
+# 控制台参数输入
+parser = argparse.ArgumentParser()
+
+# 添加参数
+parser.add_argument('-o', metavar='文章输出路径', type=str, required=False, default='./output/')
+parser.add_argument('-u', metavar='自己的CSDN账户名', type=str, required=True)
+parser.add_argument('-c', metavar='CSDN的登录之后的cookie{UserToken和UserInfo}', type=str, required=True,
+                    help='可以在CSDN的网站下打开浏览器的开发者模式，'
+                         '访问自己的文章页面，查看请求的header得到。字符串不能存在空格！')
+
+try:
+    # 参数解析
+    args = parser.parse_args()
     csdn = CSDN()
-
-    # 输出路径
-    # csdn.output = './'
-    # 输入自己的CSDN账户名
-    # csdn.user = ''
-    # 自己的登录之后的cookie,即UserToken和UserInfo的值，这两个的值都是一样的
-    # csdn.init_header('')
-
+    csdn.output = args.o
+    csdn.user = args.u
+    csdn.init_header(args.c)
     csdn.run()
+
+except argparse.ArgumentError:
+    parser.print_help()
--- README.md
+++ README.md
@@ -1,19 +1,28 @@
 # CSDN_Blog_Format
 ---
-将自己CSDN的文章转化为其编辑器里面的markdown格式
+将自己CSDN的文章转化为Hexo里面的markdown格式
 ---
-使用方法
-1. 初始化对象
- - csdn = CSDN()
+运行次脚本的环境：
+1. python3.x
+2. 所需python库:lxml,logger
+---
+使用方法其一，直接在修改代码
+
+1. 找到注释掉的代码，将注释去掉
 2. 设置输出路径
  - csdn.output = './'
 3. 设置自己的CSDN账户名
  - csdn.user = ''
 4. 设置自己的登录之后的cookie，可以自己点击一篇自己的文章编辑查看url的header得到，找到UserToken和UserInfo其中一个的值，这两个的值都是一样的
  - csdn.init_header('')
-5. 运行
- - csdn.run()
+5. 运行脚本
+---
+
+使用方法其二，运行命令行
+
+CSDN.py [-h] [-o 文章输出路径] -u 自己的CSDN账户名 -c  CSDN的登录之后的cookie{UserToken和UserInfo}
 
 ---
+使用方法其三
 如果不想要设置cookie，可以查看该项目的分支https://github.com/ddddyyyy/CSDN_Blog_Format/tree/login
 可以通过使用账号密码登录
